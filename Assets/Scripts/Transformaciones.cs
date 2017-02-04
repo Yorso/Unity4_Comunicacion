b3b5e841209672dadfdbd5bae0b5f5c3a67b58cf
Yorso <jorgeguzon@gmail.com>
2017-02-04 19:32:01 +0100

    Changes

---
+++ Assets/Scripts/Transformaciones.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transformaciones : MonoBehaviour {
+
+	//Creamos una referencia del componente Transform (optimización)
+	//1º Acceso a un Game Object de manera local
+	private Transform thisTransform = null; //Si hacemos aquí la asignación (private Transform thisTransform = GetComponent<Transform>();) 
+	                                        //puede dar error porque el componente aún puede no estar cargado, por eso lo hacemos en el Start
+
+	//2º Acceso a un Game Object de manera externa
+	public GameObject otroGO;
+	Transform transformOtroGo;
+
+	// Use this for initialization
+	void Start () {
+		//1º Acceso a un Game Object de manera local
+		thisTransform = GetComponent<Transform>();//Aquí ya podemos hacer la asignación, nos hemos asegurado al estar ya en Start que el componente ya está cargado
+		thisTransform.position = new Vector3(2, 2, 2); //Pone el GameObject en la posición 2,2,2
+
+		//2º Acceso a un Game Object de manera externa
+		otroGO = GameObject.Find ("CuboRojo");
+		transformOtroGo = otroGO.GetComponent<Transform>();
+		transformOtroGo.position = new Vector3 (-2,-2,-2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UnObjeto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnObjeto : MonoBehaviour {
+	//Ejercicio: Todos los objetos que tengan el componente Pared, que se almacenen en un array
+	public Pared[] arrayDeParedes; 
+
+	// Use this for initialization
+	void Start () {
+		arrayDeParedes = Object.FindObjectsOfType (typeof(Pared)) as Pared[]; //"as Pared[]" es como se propone que se haga la devolución de los objetos Pared que encuentre
+																		      //Encuentra los objetos y los devuelve como un array de Pared (as Pared[])
+		Debug.Log (arrayDeParedes.Length);
+		Debug.Log (arrayDeParedes.ToString()); //Nos muestra en Unity que es un array de tipo Pared => Pared[]
+	}
+
+}
