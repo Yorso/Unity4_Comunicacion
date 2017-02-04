29e819b92eb2a87ff29db4e7afd882d5b8ef7cfc
Yorso <jorgeguzon@gmail.com>
2017-02-04 23:53:06 +0100

    changes

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+
+	private Transform transform = null;
+	public float velocidad;
+
+	// Use this for initialization
+	void Start () {
+		transform = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position += new Vector3 (0, 0, 1); //Cada frame se le suma 1 a la z == avanza
+		//transform.position += new Vector3 (0,0,1 * velocidad); //Aquí velocidad es cero, podemos controlarlo desde el Inspector con el ratón
+		transform.position += new Vector3 (0,0,1 * velocidad * Time.deltaTime); //Movemos unidades por segundo => si velocidad = 10 entonces se mueve 10 unidades por segundo
+	}
+}
---
+++ Assets/Scripts/RotacionTraslacion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionTraslacion : MonoBehaviour {
+
+	private Transform transform = null;
+	public float velocidad;
+	public float velocidadRotacion;
+
+	// Use this for initialization
+	void Start () {
+		transform = GetComponent<Transform> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate (Vector3.forward * velocidad * Time.deltaTime);//Se despalza en el plano, parece igual que transform.position
+		//transform.Rotate (Vector3.up * velocidadRotacion * Time.deltaTime);//El Game Object hace círculos
+		transform.Translate (Vector3.forward * -velocidad * Time.deltaTime);
+		transform.Rotate (-Vector3.up * velocidadRotacion * Time.deltaTime);
+	}
+}
